051cc0407c486d45daf2372d3803d6b4a5fff4b9
boxty <boxty@boxtyui-MacBookPro.local>
2026-01-27 13:39:53 +0900

    buildAndScan에서 .jar 파일 확인

--- vars/buildAndScan.groovy
+++ vars/buildAndScan.groovy
@@ -1,5 +1,20 @@
-def call(service) {
+def call(String service) {
+
     def image = "${env.ECR_REGISTRY}/goorm-${service}:${env.IMAGE_TAG}"
-    sh "docker build -t ${image} service/${service}"
-    //sh "trivy image --severity HIGH,CRITICAL --exit-code 1 ${image}"
+
+    echo "🚀 Build & Dockerize service: ${service}"
+
+    sh """
+        ./gradlew :service:${service}:clean :service:${service}:bootJar
+
+        echo "📦 JAR 확인"
+        ls -al service/${service}/build/libs
+
+        docker build -t ${image} service/${service}
+
+        echo "🐳 Docker image built: ${image}"
+    """
+
+    // 보안 스캔 (필요 시 활성화)
+    // sh "trivy image --severity HIGH,CRITICAL --exit-code 1 ${image}"
 }
