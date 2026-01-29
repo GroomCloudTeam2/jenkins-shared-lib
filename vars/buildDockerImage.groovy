6d640e1d8ecd48c8a2b8fb4570c1cda2dc19b8bb
yyytgf123 <hyunho3445@gmail.com>
2026-01-29 12:42:49 +0900

    fix: dockerfile path

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,16 +1,13 @@
 def call(String service) {
+    def contextPath = "service/${service}"
+
     sh """
-        # BuildKit 활성화
         export DOCKER_BUILDKIT=1
-        
-        # 이전 이미지를 캐시로 pull (없어도 에러 안 남)
         docker pull ${env.ECR_REGISTRY}/goorm-${service}:latest || true
-        
-        # 캐시 사용하여 빌드
-        docker build \\
-            --cache-from ${env.ECR_REGISTRY}/goorm-${service}:latest \\
-            -t ${env.ECR_REGISTRY}/goorm-${service}:${env.IMAGE_TAG} \\
-            -t ${env.ECR_REGISTRY}/goorm-${service}:latest \\
-            .
+        docker build \
+            --cache-from ${env.ECR_REGISTRY}/goorm-${service}:latest \
+            -t ${env.ECR_REGISTRY}/goorm-${service}:${env.IMAGE_TAG} \
+            -t ${env.ECR_REGISTRY}/goorm-${service}:latest \
+            ${contextPath}
     """
 }
