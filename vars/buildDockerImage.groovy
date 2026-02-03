0c6dc693821891fe2fdd96c0a6f763a617f692c4
boxty <boxty@boxtyui-MacBookPro.local>
2026-02-03 14:57:11 +0900

    mac용 이미지

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,13 +1,35 @@
-def call(String service) {
+//def call(String service) {
+//    def contextPath = "service/${service}"
+//
+//    sh """
+//        export DOCKER_BUILDKIT=1
+//        docker pull ${env.ECR_REGISTRY}/goorm-${service}:latest || true
+//        docker build \
+//            --cache-from ${env.ECR_REGISTRY}/goorm-${service}:latest \
+//            -t ${env.ECR_REGISTRY}/goorm-${service}:${env.IMAGE_TAG} \
+//            -t ${env.ECR_REGISTRY}/goorm-${service}:latest \
+//            ${contextPath}
+//    """
+//}
+def call(String service, String imageTag) {
     def contextPath = "service/${service}"
+    def imageName = "${env.ECR_REGISTRY}/goorm-${service}"
 
     sh """
-        export DOCKER_BUILDKIT=1
-        docker pull ${env.ECR_REGISTRY}/goorm-${service}:latest || true
-        docker build \
-            --cache-from ${env.ECR_REGISTRY}/goorm-${service}:latest \
-            -t ${env.ECR_REGISTRY}/goorm-${service}:${env.IMAGE_TAG} \
-            -t ${env.ECR_REGISTRY}/goorm-${service}:latest \
+        set -e
+
+        # buildx 사용 보장
+        docker buildx inspect multiarch-builder >/dev/null 2>&1 || \
+        docker buildx create --name multiarch-builder --use
+
+        docker buildx use multiarch-builder
+
+        # 멀티 아키텍처 빌드 & ECR push
+        docker buildx build \
+            --platform linux/amd64,linux/arm64 \
+            -t ${imageName}:${imageTag} \
+            -t ${imageName}:latest \
+            --push \
             ${contextPath}
     """
 }
