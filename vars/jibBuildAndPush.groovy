fffdcb7ec9bed5a4948ff63b57eaab1d3ec89496
boxty <boxty@boxtyui-MacBookPro.local>
2026-02-06 17:16:06 +0900

    jib 권한

--- vars/jibBuildAndPush.groovy
+++ vars/jibBuildAndPush.groovy
@@ -8,21 +8,19 @@ def call(Map args = [:]) {
         return
     }
 
-    if (!env.ECR_PASSWORD) {
-        error("ECR_PASSWORD is not set. Did you run ECR Login stage?")
-    }
+    // [수정] IRSA 사용 시 Password 체크 로직이 필요 없으므로 제거
 
     parallel services.collectEntries { svc ->
         [(svc): {
-            def image = "${ecrRegistry}/goorm-${svc}:${imageTag}"
-            echo "Jib build & push -> ${image}"
+            // ECR 레포지토리 이름 규칙 확인 (goorm인지 courm인지 확인 필요)
+            def image = "${ecrRegistry}/courm-${svc}:${imageTag}"
+            echo "Jib build & push using IRSA -> ${image}"
 
             sh """
               set -e
               ./gradlew :service:${svc}:jib --no-daemon \\
                 -Djib.to.image=${image} \\
-                -Djib.to.auth.username=AWS \\
-                -Djib.to.auth.password=${env.ECR_PASSWORD}
+                -Djib.to.credHelper=ecr-login
             """
         }]
     }
