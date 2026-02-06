abff749f02f1e92c42c6160f6456b2d5ea203b04
boxty <boxty@boxtyui-MacBookPro.local>
2026-02-06 14:26:54 +0900

    aws cli 제거

--- vars/jibBuildAndPush.groovy
+++ vars/jibBuildAndPush.groovy
@@ -1,16 +1,13 @@
 def call(Map args = [:]) {
     // 필수 파라미터
-    def services     = args.services ?: error("services is required")
-    def imageTag     = args.imageTag ?: error("imageTag is required")
-    def ecrRegistry  = args.ecrRegistry ?: error("ecrRegistry is required")
-    def awsRegion    = args.awsRegion ?: error("awsRegion is required")
+    def services    = args.services ?: error("services is required")
+    def imageTag    = args.imageTag ?: error("imageTag is required")
+    def ecrRegistry = args.ecrRegistry ?: error("ecrRegistry is required")
 
     if (!services || services.isEmpty()) {
         echo "No services to build & push"
         return
     }
-
-    sh "aws sts get-caller-identity || true"
 
     parallel services.collectEntries { svc ->
         [(svc): {
@@ -19,13 +16,8 @@ def call(Map args = [:]) {
 
             sh """
                 set -e
-                set +x
-                ECR_PW=\$(aws ecr get-login-password --region ${awsRegion})
-
                 ./gradlew :service:${svc}:jib --no-daemon \\
-                  -Djib.to.image=${image} \\
-                  -Djib.to.auth.username=AWS \\
-                  -Djib.to.auth.password=\$ECR_PW
+                  -Djib.to.image=${image}
             """
         }]
     }
