08687131b6e686564bb0c419e02ccbed5eec02aa
boxty <boxty@boxtyui-MacBookPro.local>
2026-02-10 13:34:54 +0900

    이중 병렬 제거

--- vars/runServiceTests.groovy
+++ vars/runServiceTests.groovy
@@ -1,7 +1,7 @@
 def call(Map args = [:]) {
     def services     = args.services ?: error("services is required")
     def excludeTags  = args.excludeTags ?: ""
-    def gradleOpts   = args.gradleOpts ?: "--no-daemon --parallel"
+    def gradleOpts   = args.gradleOpts ?: "--no-daemon"
 
     if (!services || services.isEmpty()) {
         echo "No services to test"
